3b63b7209b250e5f850ef29510464a8d4fc45806
iyanu456 <oyerindei13@gmail.com>
2025-07-19 14:04:58 +0100

    working on dashboard

--- frontend/app/dashboard/tabs/HomeTab.jsx
+++ frontend/app/dashboard/tabs/HomeTab.jsx
@@ -61,7 +61,7 @@ export default function HomeTab() {
         </div>
       </section>
 
-      <section className="grid grid-cols-[60%_40%] w-full h-full gap-8">
+      <section className="grid grid-cols-[50%_47%] w-full h-full gap-8">
         <section className=" w-full h-full">
           <div className="bg-secondary w-full h-fit p-6 rounded-2xl flex justify-between">
             <div className="space-y-2">
@@ -84,10 +84,10 @@ export default function HomeTab() {
 
 
 
-            <div className="relative grid grid-cols-3 gap-[7em] mt-[4em] h-fit place-items-center z-50">
+            <div className="relative mt-3 grid grid-cols-3 gap-[7vw]  h-fit place-items-center z-50">
             {lessons.map((lesson, index) =>
               lesson.current ? (
-              <div className="flex flex-col gap-2 items-center mt-[-3em] " key={index}>
+              <div className="flex flex-col gap-2 items-center  z-50" key={index}>
                   <button className="" key={index}>
                     <Image
                       src="/level.png"
@@ -103,7 +103,7 @@ export default function HomeTab() {
                   </div>
                 </div>
               ) : (
-                <div className="flex flex-col gap-2 items-center mt-[-3em]" key={index}>
+                <div className="flex flex-col gap-2 items-center z-50" key={index}>
                   <button className="" key={index}>
                     <Image
                       src="/level.png"
@@ -120,42 +120,47 @@ export default function HomeTab() {
                 </div>
               )
             )}
+
+            <div className="z-20 absolute w-full h-full">
+              <Image src={"/path.svg"} fill alt="image" className="mt-[-1.3em]"
+              />
+            </div>
           </div>
 
 
 
 
-          <div className="absolute w-full h-full top-0 z-40 grid grid-cols-3">
+         {/* <div className="absolute w-full h-full top-0 z-40 grid grid-cols-3">
             <div className="grid  place-items-center h-full w-full items-center ">
               <hr className="border-t-3 border-gray-400 border-dashed w-[35%] mr-0 ml-auto"></hr>
             </div>
 
-            <div className="grid place-items-center h-full w-full  items-center">
+            {/*<div className="grid place-items-center h-full w-full  items-center">
               <hr className="border-t-3 border-gray-400 border-dashed w-full"></hr>
               
-            </div>
+            </div>/}
 
-            <div className="grid place-items-center  rounded-br-lg h-full w-[90%] mb-0 mt-auto items-center">
+            {/*<div className="grid place-items-center  rounded-br-lg h-full w-[90%] mb-0 mt-auto items-center">
               <hr className="border-[0px] h-[0px] border-gray-400 border-dashed w-1/2 ml-0 mr-auto opacity-0"></hr>
               <div className=" border-r-3 border-dashed w-full border-t-3 h-full border-gray-400 rounded-tr-lg mr-0 ml-auto"></div>
-            </div>
+            </div>/}
 
             
 
-             {/*-----------------------------------------*/}
+             {/-----------------------------------------/}
 
-              <div className=" w-[56%] mr-2 ml-auto h-full "></div>
+             {/* <div className=" w-[56%] mr-2 ml-auto h-full "></div>
               <div className=" w-full h-full border-gray-400 border-b-3 mr-2 border-dashed"></div>
               <div className=" w-[90%] border-gray-400 ml-0 mr-auto h-full  border-b-3  border-r-3 rounded-br-lg border-dashed mt-[-0.1px]"></div>
+             /}
 
 
-
-            {/*-----------------------------------------*/}
+            {/-----------------------------------------/}
 
 
 
             <div className="grid place-items-center h-[53.35%] w-[58%] border-t-3 border-b-3 border-gray-400 border-dashed border-l-3 rounded-tl-lg mt-[-0.14em] mr-0 ml-auto items-center ">
-              {/*<hr className="border-2 border-gray-400 border-dashed w-1/2 mr-0 ml-auto"></hr>*/}
+              {/*<hr className="border-2 border-gray-400 border-dashed w-1/2 mr-0 ml-auto"></hr>/}
             </div>
 
             <div className="grid place-items-center h-[6em] mt-[2em] w-full items-center">
@@ -167,12 +172,16 @@ export default function HomeTab() {
             </div>
             
             
-          </div>
+          </div>*/}
           </div>
           
         </section>
 
-        <section className=""></section>
+        <section className="flex flex-col">
+          <div className="flex gap-3 rounded-[0.9em] font-baloo font-bold px-[2em] py-[1em] text-2xl bg-[#ffffff5b] border-2 border-[#ffffff52] w-full h-fit">
+            <Image src={ "/menu.svg"} alt="menu" height={28} width={28} />Topic</div>
+        </section>
+        
       </section>
     </div>
   );
